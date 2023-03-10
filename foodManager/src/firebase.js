744f8ba4f21e71937d42fe36c602164b46be41f8
demitan02 <97874900+demitan02@users.noreply.github.com>
2023-03-10 15:19:09 +0800

    Create firebase.js
    firebase.js authentication stuff

---
+++ foodManager/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdTJ0xrY4Sc1Tu_kdQAAqMw6eIKYoS9ao",
+  authDomain: "foodmanager-c0afa.firebaseapp.com",
+  projectId: "foodmanager-c0afa",
+  storageBucket: "foodmanager-c0afa.appspot.com",
+  messagingSenderId: "372272255912",
+  appId: "1:372272255912:web:79ad46103b5e118249439d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
