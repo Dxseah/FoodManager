839388b384ae93a2916d00ff8c85af9785559421
demitan02 <97874900+demitan02@users.noreply.github.com>
2023-03-20 00:20:11 +0800

    added table in for profile page, need to link

--- foodManager/src/components/Pages/ProfilePage.vue
+++ foodManager/src/components/Pages/ProfilePage.vue
@@ -3,17 +3,37 @@
 
           <div v-if="user.loggedIn">
 
-          <div class="card-header">Welcome, {{user.data.displayName}}!</div>
-          <div class="card-body">
-            <div class="alert alert-success" role="alert">
-            You are logged in!
-            <div class="my-4">
+            <div class="card-header">Welcome, {{user.data.displayName}}!</div>
+            <div class="card-body">
+              <div class="alert alert-success" role="alert">
+                You are logged in!
+                <div class="my-4">
                   <button @click.prevent="signOut" class="btn btn-primary">Log Out</button>
+                </div>
+              </div>
             </div>
-             </div>
-          </div>
+
+            <table id="currTable">
+                <tr>
+                    <th>First Name</th>
+                    <td> sample name </td>
+                </tr>
+                <tr>
+                    <th>Last Name</th>
+                    <td> sample last name </td>
+                </tr>
+                <tr>
+                    <th>Email</th>
+                    <td> sample email </td>
+                </tr>
+                <tr>
+                    <th>User ID</th>
+                    <td> sample user id </td>
+                </tr>
+            </table>
 
           </div>
+
             <div v-else class="alert alert-danger" role="alert">
               You are not logged in! 
             </div>
@@ -64,4 +84,19 @@ export default {
     font-family: Avenir, Arial, Helvetica, sans-serif;
     font-size: 20px;
   }
+
+#currTable {
+  text-align: left;
+  width: 100%;
+  border-collapse: collapse;
+  border: 1px solid;
+}
+
+tr, td {
+  border: 1px solid;
+}
+
+th {
+  font-weight: bold;
+}
 </style>
