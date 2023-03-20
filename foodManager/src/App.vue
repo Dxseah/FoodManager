167d8dd5fe9ccd73a9453586c694bd53bf9aed77
Dxseah <64683658+Dxseah@users.noreply.github.com>
2023-03-20 14:56:16 +0800

    Update App.vue
    user views different pages according to login status

--- foodManager/src/App.vue
+++ foodManager/src/App.vue
@@ -2,7 +2,9 @@
 
 <script>
   import router from '@/components/Router/index.js'
+  import {useStore} from "vuex"
   import { auth } from './firebase.js'
+  import {computed} from "vue";
   import { signOut,onAuthStateChanged } from 'firebase/auth'
   export default {
   name: 'App',
@@ -12,37 +14,56 @@
       displayName: ''
     }
   },
-  beforeUpdate() {
-    if (auth.currentUser) {
-      console.log('here')
-      this.isLoggedIn = true
-      this.displayName = auth.currentUser.displayName
-    }
-  },
-  methods: {
-    signOut() {
-      signOut(auth)
-      .then(() => {
-        console.log('signed out')
-        this.isLoggedIn = false
+  setup() {
+    const store = useStore()
+    auth.onAuthStateChanged(user => {
+    store.dispatch("fetchUser", user);
+  });
+    const user = computed(() => {
+    return store.getters.user;
+  });
+  const signOut = async () => {
+        await store.dispatch('logOut')
         router.push('/')
-      })
-    }
   }
+
+    return {user,signOut}
+ },
+  // beforeUpdate() {
+  //   if (auth.currentUser) {
+  //     console.log('here')
+  //     this.isLoggedIn = true
+  //     this.displayName = auth.currentUser.displayName
+  //   }
+  // },
+  // methods: {
+  //   signOut() {
+  //     signOut(auth)
+  //     .then(() => {
+  //       console.log('signed out')
+  //       this.isLoggedIn = false
+  //       router.push('/')
+  //     })
+  //   }
+  // }
 }
 </script>
 
 <template>
 <nav>
-  <div id="nav">
-    |<router-link to = "/"> Login </router-link>|
+  <div v-if="user.loggedIn" id="nav">
+    
     <router-link to = "/donorhome"> Donor Home </router-link>|
     <router-link to = "/beneficiaryhome"> Beneficiary Home </router-link>|
     <router-link to = "/profile"> Profile </router-link>|
     <router-link to = "/about"> About Us </router-link>|
     <!-- isLoggedIn is buggy, sometimes displayName doesn't load after refreshing page -->
-    <h1 v-if=isLoggedIn id="displayName">Welcome, {{auth.currentUser.displayName}}!</h1>
-    <h1 v-else id="displayName">Welcome, user!</h1>
+    <h1 id="displayName">Welcome, {{user.data.displayName}}!</h1>
+  </div>
+  <div v-else id="nav">
+    |<router-link to = "/"> Login </router-link>|
+    <router-link to = "/about"> About Us </router-link>|
+    <h1 id="displayName">Welcome, user!</h1>
   </div>
   <router-view/>
 </nav>
