66bd447eb16cc7ca302e8364b7ace8e3b86dac9d
demitan02 <97874900+demitan02@users.noreply.github.com>
2023-03-22 23:19:14 +0800

    implement lect method of auth
    for profile page + app

--- foodManager/src/App.vue
+++ foodManager/src/App.vue
@@ -1,18 +1,18 @@
 <template>
 <nav>
-  <div v-if="user.loggedIn" id="nav">
+  <div v-if="user" id="nav">
     <!-- <p> {{user.data}}</p> -->
     <router-link to = "/donorhome"> Donor Home </router-link>|
     <router-link to = "/beneficiaryhome"> Beneficiary Home </router-link>|
     <router-link to = "/profile"> Profile </router-link>|
     <router-link to = "/about"> About Us </router-link>|
     <!-- isLoggedIn is buggy, sometimes displayName doesn't load after refreshing page -->
-    <h1 id="displayName">Welcome, {{user.data.displayName}}!</h1>
+    <h1 id="displayName">Welcome, {{user.displayName}}!</h1>
   </div>
   <div v-else id="nav">
     |<router-link to = "/"> Login </router-link>|
     <router-link to = "/about"> About Us </router-link>|
-    <h1 id="displayName">Welcome, user!</h1>
+    <h1 id="displayName">Welcome!</h1>
   </div>
   <router-view :key="$route.fullPath"/> 
 </nav>
@@ -20,41 +20,68 @@
 
 <script>
   import router from '@/components/Router/index.js'
-  import { useStore } from "vuex"
-  import firebaseApp, { auth } from './firebase.js'
-  import { computed } from "vue";
-import { onAuthStateChanged } from 'firebase/auth';
+  // import { useStore } from "vuex"
+  // import { computed } from "vue";
+  import firebaseApp from '@/firebase.js';
+  import { getAuth, onAuthStateChanged } from "firebase/auth";
 
   export default {
-  name: 'App',
-  setup() {
-    const store = useStore()
-    auth.onAuthStateChanged(user => {
-    console.log('here3')
-    store.dispatch("fetchUser", user);
-  });
-    const user = computed(() => {
+    name: "App", 
+    data() {
+      return {
+        user: false,
+      }
+    }, 
 
-    return store.getters.user;
-  });
-  const signOut = async () => {
-        await store.dispatch('logOut')
-        router.push('/')
+    mounted() {
+      const auth = getAuth();
+      onAuthStateChanged(auth,(user)=>{
+        if (user) {
+          this.user = user;
+        }
+      })
+    }
   }
-    return {user,signOut}
- },
-}
+
+  // export default {
+  // name: 'App',
+//   setup() {
+//     const store = useStore()
+//     auth.onAuthStateChanged(user => {
+//     console.log('here3')
+//     store.dispatch("fetchUser", user);
+//   });
+//     const user = computed(() => {
+
+//     return store.getters.user;
+//   });
+//   const signOut = async () => {
+//         await store.dispatch('logOut')
+//         router.push('/')
+//   }
+//     return {user,signOut}
+//  },
+// }
 
 // // copy to all the pages
-// import firebaseApp from '@/firebase.js'
-// import (getAuth) from "firebase/auth";
-// mounted() {
-//   const auth = getAuth();
-//   onAuthStateChanged(auth,(user)=>{
-//     if (user) {
-//       this.user =user;
+// import firebaseApp from '@/firebase.js';
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+// export default {
+//   name: "ProfilePage", 
+//   data() {
+//     return {
+//       user: false,
 //     }
-//   })
+//   }, 
+
+//   mounted() {
+//     const auth = getAuth();
+//     onAuthStateChanged(auth,(user)=>{
+//       if (user) {
+//         this.user = user;
+//       }
+//     })
+//   }
 // }
 </script>
 
--- foodManager/src/components/Pages/ProfilePage.vue
+++ foodManager/src/components/Pages/ProfilePage.vue
@@ -1,90 +1,118 @@
 <template>
   <div class="container">
 
-        <div v-if="user.loggedIn">
-          
-            <div class="card-body">
-              <div class="alert alert-success" role="alert">
-                You are logged in!
-                <div class="my-4">
-                  <button @click.prevent="signOut" class="btn btn-primary">Log Out</button>
-                </div>
-              </div>
+    <div v-if="user">
+      <div class="card-body">
+        <div class="alert alert-success" role="alert">
+          You are logged in!
+            <div class="my-4">
+                  <!-- <button @click.prevent="signOut" class="btn btn-primary">Log Out</button> -->
+              <button id="btn" @click="signOut()" v-if="user"> Log Out </button>
             </div>
-
-            <table id="currTable">
-                <tr>
-                    <th>Name</th>
-                    <td> {{user.data.displayName}} </td>
-                </tr>
-                <tr>
-                    <th>User ID</th>
-                    <td> {{user.data.userID}} </td>
-                </tr>
-                <tr>
-                    <th>Email</th>
-                    <td> {{user.data.email}} </td>
-                </tr>
-                <tr>
-                    <th>Contact</th>
-                    <td> {{user.data.contact}} </td>   
-                </tr>
-                <tr>
-                    <th>Account Type</th>
-                    <td v-if="user.data.account==2"> Donor </td>
-                    <td v-else-if="user.data.account==1"> Beneficiary </td>
-                </tr>
-
-            </table>
-          
         </div>
-        <div v-else class="alert alert-danger" role="alert">
-          You are not logged in! 
-        </div>
+      </div>
+
+      <table id="currTable">
+        <tr>
+          <th>Name</th>
+          <td> {{user.displayName}} </td>
+        </tr>
+        <tr>
+          <th>User ID</th>
+          <td> {{user.uid}} </td>
+        </tr>
+        <tr>
+          <th>Email</th>
+          <td> {{user.email}} </td>
+        </tr>
+        <tr>
+          <th>Contact</th>
+          <td> sample contact </td>   
+        </tr>
+        <!-- <tr>
+          <th>Account Type</th>
+          <td v-if="user.data.account==2"> Donor </td>
+          <td v-else-if="user.data.account==1"> Beneficiary </td>
+        </tr> -->
+
+      </table>
+    </div>
+    <div v-else class="alert alert-danger" role="alert">
+      You are not logged in!
+    </div>
   </div>
 </template>
 
 <script>
-import { useStore } from 'vuex';
-import { useRouter } from 'vue-router';
-import { ref , computed } from 'vue';
-import { auth } from '@/firebase.js'
-
-
-
+import firebaseApp from '@/firebase.js';
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 export default {
-  name: "ProfilePage",
-
-  setup() {
-
-    const store = useStore()
-    const router = useRouter()
-    auth.onAuthStateChanged(user => {
-      store.dispatch("fetchUser", user);
-    });
-    const user = computed(() => {
-      store.dispatch("fetchUser", auth.currentUser)
-      return store.getters.user;
-    });
-
-    const signOut = async () => {
-          await store.dispatch('logOut')
-          router.push('/')
-    }
-
-    return { user, signOut }
- },
+  name: "ProfilePage", 
   data() {
     return {
-      displayName: ''
-    };
-  },
-  beforeCreate() {
-    console.log('test')
-    
-  }
+      user: false,
+    }
+  }, 
 
-};
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth,(user)=>{
+      if (user) {
+        this.user = user;
+      }
+    })
+  }, 
+
+  methods: {
+    signOut() {
+      const auth = getAuth(); 
+      const user = auth.currentUser; 
+      signOut(auth, user);
+      this$router.push({name:'Login'})
+    }
+  }
+}
+
+
+
+// import { useStore } from 'vuex';
+// import { useRouter } from 'vue-router';
+// import { ref , computed } from 'vue';
+// import { auth } from '@/firebase.js'
+
+// export default {
+//   name: "ProfilePage",
+
+//   setup() {
+
+//     const store = useStore()
+//     const router = useRouter()
+//     auth.onAuthStateChanged(user => {
+//       store.dispatch("fetchUser", user);
+//     });
+//     const user = computed(() => {
+//       store.dispatch("fetchUser", auth.currentUser)
+//       return store.getters.user;
+//     });
+
+//     const signOut = async () => {
+//           await store.dispatch('logOut')
+//           router.push('/')
+//     }
+
+//     return { user, signOut }
+//  },
+//   data() {
+//     return {
+//       displayName: ''
+//     };
+//   },
+//   beforeCreate() {
+//     console.log('test')
+    
+//   }
+
+// };
 </script>
 
 <style scoped>
