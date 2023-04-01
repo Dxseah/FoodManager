5ca916b80b52d949cc61a19d418c08e85e8ddb7a
Brian Liew <brianlw1872@gmail.com>
2023-04-01 15:28:23 +0800

    fixed the donorform upload alert issue

--- foodManager/src/components/Pages/DonorFormPage.vue
+++ foodManager/src/components/Pages/DonorFormPage.vue
@@ -3,7 +3,7 @@
     <div class="transbox">
       <div class="content">
         <h1 class="header">Donor Form</h1>
-        <form class="form"  @submit.prevent="submitForm">
+        <form class="form" @submit.prevent="submitForm">
           <div class="form-group">
             <label for="rice">Rice</label>
             <input type="number" id="rice" v-model.number="riceQuantity" min="0" />
@@ -18,7 +18,7 @@
           </div>
           <div class="form-group">
             <label for="image-upload">Upload Image</label>
-            <input type="file" id="image-upload" @change="handleImageUpload" required/>
+            <input type="file" id="image-upload" @change="handleImageUpload" required />
           </div>
           <button class="submit-button" v-on:click="submitAlert">Submit Donation</button>
         </form>
@@ -28,11 +28,8 @@
 </template>
 
 <script>
-// import { ref } from 'vue'
-// import { useStore } from 'vuex'
-// import router from '@/components/Router/index.js'
-import { db } from '@/firebase'
-import { getDoc, doc, updateDoc, setDoc, collection } from "firebase/firestore"
+import { db } from "@/firebase";
+import { getDoc, doc, setDoc, collection } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default {
@@ -48,56 +45,55 @@ export default {
 
   mounted() {
     const auth = getAuth();
-    onAuthStateChanged(auth,(user)=>{
+    onAuthStateChanged(auth, (user) => {
       if (user) {
         this.user = user;
       }
-    })
-  }, 
+    });
+  },
 
   methods: {
     async submitForm() {
       try {
-      const auth = getAuth(); 
-      const user = auth.currentUser;
+        const auth = getAuth();
+        const user = auth.currentUser;
 
-      // Save data to Firestore
-      // const userId = store.getters['auth/user'].id;
-      const foodItemRef = collection(db, 'DonatedFood');
-      const docRef = doc(foodItemRef, user.email); //changed this
-      const docSnap = await getDoc(docRef);
-      const donationData = {
-        rice: this.riceQuantity,
-        cannedFood: this.cannedFoodQuantity,
-        instantNoodles: this.instantNoodlesQuantity,
-        // timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-        // userEmail: user.email
-      };
-      if (docSnap.exists()) {
-        await setDoc(docRef, donationData, { merge: true });
-      } else {
-        await setDoc(docRef, donationData);
+        // Save data to Firestore
+        const foodItemRef = collection(db, "DonatedFood");
+        const docRef = doc(foodItemRef, user.email);
+        const docSnap = await getDoc(docRef);
+        const donationData = {
+          rice: this.riceQuantity,
+          cannedFood: this.cannedFoodQuantity,
+          instantNoodles: this.instantNoodlesQuantity,
+          // timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+          // userEmail: user.email
+        };
+        if (docSnap.exists()) {
+          await setDoc(docRef, donationData, { merge: true });
+        } else {
+          await setDoc(docRef, donationData);
+        }
+        if (!this.imageFile) {
+        alert("Please upload an image of your donation.");
+        return;
       }
-      console.log("Form submitted")
-
-    }
-    catch (err) {
-        alert(err.message)
+      console.log("Form submitted");
+    } catch (err) {
+      alert(err.message);
     }
   },
-
   async submitAlert() {
     if (!this.imageFile) {
-    alert("Please upload an image of your donation.");
-    return;
+      alert("Please upload an image of your donation.");
+      return;
     }
     alert("Donation Form is submitted!")
+  },
+  handleImageUpload(event) {
+    this.imageFile = event.target.files[0];
   }
-
-    // handleImageUpload(event) {
-    //     this.imageFile = event.target.files[0];
-    // } 
-  }
+}
 }
 </script>
 
