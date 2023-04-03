20b93ffc2a8d744aa96c0efffe1c04362bbd2abf
Dxseah <64683658+Dxseah@users.noreply.github.com>
2023-04-04 00:58:15 +0800

    Update App.vue

--- foodManager/src/App.vue
+++ foodManager/src/App.vue
@@ -1,22 +1,20 @@
 <template>
-<nav id='nav'>
-  <router-link to = "/about"> About Us </router-link>|
-  <div v-if="!user">
-    <h1 id="displayName">Welcome!</h1>
-    <router-link to = "/"> Login </router-link>|
-  </div>
-  <div v-else-if="user && account == 'Donor'">
+<nav>
+  <div v-if="user" id="nav">
+    <!-- <p> {{user.data}}</p> -->
     <router-link to = "/donorhome"> Donor Home </router-link>|
-    <router-link to = "/donorprofile"> Donor Profile </router-link>
-    <h1 id="displayName">Welcome, {{user.displayName}}!</h1>
-  </div>
-  <div v-else-if="user && account=='Admin'">
-  </div>
-  <div v-else>
     <router-link to = "/beneficiaryhome"> Beneficiary Home </router-link>|
+    <router-link to = "/adminhome"> Admin Home </router-link> |
+    <router-link to = "/donorprofile"> Donor Profile </router-link>|
     <router-link to = "/beneficiaryprofile"> Beneficiary Profile </router-link>|
+    <router-link to = "/about"> About Us </router-link>|
+    <!-- isLoggedIn is buggy, sometimes displayName doesn't load after refreshing page -->
     <h1 id="displayName">Welcome, {{user.displayName}}!</h1>
-
+  </div>
+  <div v-else id="nav">
+    |<router-link to = "/"> Login </router-link>|
+    <router-link to = "/about"> About Us </router-link>|
+    <h1 id="displayName">Welcome!</h1>
   </div>
   <router-view :key="$route.fullPath"/> 
 </nav>
@@ -24,10 +22,11 @@
 
 <script>
   import router from '@/components/Router/index.js'
+  // import { useStore } from "vuex"
+  // import { computed } from "vue";
   import firebaseApp from '@/firebase.js';
   import { getAuth, onAuthStateChanged } from "firebase/auth";
-  import { getDoc, doc } from "firebase/firestore"
-  import { db } from '@/firebase'
+
   export default {
     name: "App", 
     data() {
