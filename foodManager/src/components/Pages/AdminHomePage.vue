921e00aec6ed7894e2d6757201ff004cdde0974c
lyihan00 <e0773663@u.nus.edu>
2023-04-03 14:07:27 +0800

    Refactor ProgressBar.vue, Update AdminHomePage.vue, BeneficiaryHomePage.vue, DonorHomePage.vue
    Set max progress bar width = 100%, sum up quantities from donatedFood and requestedFood collection on Firebase to calculate donated and requested quantities

--- foodManager/src/components/Pages/AdminHomePage.vue
+++ foodManager/src/components/Pages/AdminHomePage.vue
@@ -5,11 +5,18 @@
             <h1 class="newHeader">Food Items</h1><br>
             <router-link to="/adminform" class="button">Add Food Item</router-link>
             <h1 class="header"> Track Food Item Progress </h1>
-            <h3 class="food">{{  food }}</h3>
-            <!-- To Do: Pass in value to next 2 lines -->
-            <ProgressBar :percentComplete="percent"/>
+            <h3 class="food">Rice</h3>
+            <ProgressBar :percentComplete="displayRice"/>
             <h3 class="percent">{{ donatedRice }} / {{ requestedRice }} raised</h3>
-            <h3 class="percent"> {{ percent }}% completed</h3>
+            <h3 class="percent"> {{ percentRice }}% completed</h3>
+            <h3 class="food">Canned Food</h3>
+            <ProgressBar :percentComplete="displayCannedFood"/>
+            <h3 class="percent">{{ donatedCannedFood }} / {{ requestedCannedFood }} raised</h3>
+            <h3 class="percent"> {{ percentCannedFood }}% completed</h3>
+            <h3 class="food">Instant Noodles</h3>
+            <ProgressBar :percentComplete="displayInstantNoodles"/>
+            <h3 class="percent">{{ donatedInstantNoodles }} / {{ requestedInstantNoodles }} raised</h3>
+            <h3 class="percent"> {{ percentInstantNoodles }}% completed</h3>
         </div>
         <div class>
             <button id="saveChangesBtn">Save Changes</button>
@@ -31,28 +38,52 @@ export default {
           ProgressBar,
       },
 
-  data() {
-    return {
-      food: "Rice",
+data() {
+  return {
+    //  food: "Rice",
       donatedRice: null,
       requestedRice: null,
-      percent: null
-    };
-  },
+      percentRice: null,
+      displayRice: null,
+      donatedCannedFood: null,
+      requestedCannedFood: null,
+      percentCannedFood: null,
+      displayCannedFood: null,
+      donatedInstantNoodles: null,
+      requestedInstantNoodles: null,
+      percentInstantNoodles: null,
+      displayInstantNoodles: null,
+  }
+},
 
   async created() {
     const queryDonated = query(collection(db, 'DonatedFood'))
     const queryDonatedSnapshot = await getDocs(queryDonated);
-    const sumDonated = queryDonatedSnapshot.docs.map(doc => doc.data().rice);
-    this.donatedRice = sumDonated.reduce((acc, val) => acc + val, 0);
 
     const queryRequested = query(collection(db, 'RequestedFood'));
     const queryRequestedSnapshot = await getDocs(queryRequested);
-    const sumRequested = queryRequestedSnapshot.docs.map(doc => doc.data().rice);
-    this.requestedRice = sumRequested.reduce((acc, val) => acc + val, 0);
 
-    this.percent = parseFloat(this.donatedRice / this.requestedRice * 100).toFixed(1);
-  }
+    const sumDonatedRice = queryDonatedSnapshot.docs.map(doc => doc.data().rice);
+    this.donatedRice = sumDonatedRice.reduce((acc, val) => acc + val, 0);
+    const sumRequestedRice = queryRequestedSnapshot.docs.map(doc => doc.data().rice);
+    this.requestedRice = sumRequestedRice.reduce((acc, val) => acc + val, 0);
+    this.percentRice = parseFloat(this.donatedRice / this.requestedRice * 100).toFixed(1);
+    this.displayRice = Math.min(this.percentRice, 100);
+
+    const sumDonatedCannedFood = queryDonatedSnapshot.docs.map(doc => doc.data().cannedFood);
+    this.donatedCannedFood = sumDonatedCannedFood.reduce((acc, val) => acc + val, 0);
+    const sumRequestedCannedFood = queryRequestedSnapshot.docs.map(doc => doc.data().cannedFood);
+    this.requestedCannedFood = sumRequestedCannedFood.reduce((acc, val) => acc + val, 0);
+    this.percentCannedFood = parseFloat(this.donatedCannedFood / this.requestedCannedFood * 100).toFixed(1);
+    this.displayCannedFood = Math.min(this.percentCannedFood, 100);
+
+    const sumDonatedInstantNoodles = queryDonatedSnapshot.docs.map(doc => doc.data().instantNoodles);
+    this.donatedInstantNoodles = sumDonatedInstantNoodles.reduce((acc, val) => acc + val, 0);
+    const sumRequestedInstantNoodles = queryRequestedSnapshot.docs.map(doc => doc.data().instantNoodles);
+    this.requestedInstantNoodles = sumRequestedInstantNoodles.reduce((acc, val) => acc + val, 0);
+    this.percentInstantNoodles = parseFloat(this.donatedInstantNoodles / this.requestedInstantNoodles * 100).toFixed(1);
+    this.displayInstantNoodles = Math.min(this.percentInstantNoodles, 100);
+  },
 }
 </script>
   
--- foodManager/src/components/ProgressBar.vue
+++ foodManager/src/components/ProgressBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="progress-bar">
-      <div class="progress-bar-fill" :style="{ width: percentComplete + '%' }"></div>
+      <div class="progress-bar-fill" :style="{ width: percentComplete + '%'}"></div>
     </div>
   </template>
   
@@ -10,7 +10,7 @@
       percentComplete: {
         type: Number,
         default: 0,
-        required: true
+        required: true,
       }
     }
   };
