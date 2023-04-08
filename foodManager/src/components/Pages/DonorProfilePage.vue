7286f4bb12153ad6747313d963385c159cf74661
Dxseah <64683658+Dxseah@users.noreply.github.com>
2023-04-08 15:20:00 +0800

    Update DonorProfilePage.vue

--- foodManager/src/components/Pages/DonorProfilePage.vue
+++ foodManager/src/components/Pages/DonorProfilePage.vue
@@ -1,34 +1,55 @@
-
 <template>
   <div class="container1" v-if="user">
     <div v-if="exists">
-      <div class="card-body">
-        <div class="alert alert-success" role="alert">
-          You are logged in!
+        <div class="card-body">
+          <div class="alert alert-success" role="alert">
+            You are logged in!
+            <!-- <Logout/> -->
+          </div>
         </div>
-      </div>
 
-      <table id="currTable">
-        <tr>
-          <th>Name</th>
-          <td> {{docName}} </td>
-        </tr>
-        <tr>
-          <th>User ID</th>
-          <td> {{user.uid}} </td>
-        </tr>
-        <tr>
-          <th>Email</th>
-          <td> {{user.email}} </td>
-        </tr>
-        <tr>
-          <th>Contact</th>
-          <td> Sample Contact </td>   
-        </tr>
+        <table id="currTable">
+          <tr>
+            <th>Name</th>
+            <td> {{user.displayName}} </td>
+          </tr>
+          <tr>
+            <th>User ID</th>
+            <td> {{user.uid}} </td>
+          </tr>
+          <tr>
+            <th>Email</th>
+            <td> {{user.email}} </td>
+          </tr>
+          <tr>
+            <th>Contact</th>
+            <td> {{userData.contact}} </td>   
+          </tr>
 
       </table>
+
       <br>
-      <h1 class="header">Food Donation History</h1>
+
+      <div v-if = "account=='Donor'">
+        <!-- <h1 class="header">Donation History</h1>
+        <div v-if="donations.length === 0">You have not made any donations yet!</div>
+        <table v-else>
+          <thead>
+            <tr>
+              <th>Bags of Rice</th>
+              <th>Canned Food</th>
+              <th>Packs of Instant Noodles</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(donation, index) in donations" :key="index">
+              <td>{{ donation.rice }}</td>
+              <td>{{ donation.cannedFood }}</td>
+              <td>{{ donation.instantNoodles }}</td>
+            </tr>
+          </tbody>
+        </table> -->
+         <h1 class="header">Food Donation History</h1>
         <div v-if="donations.length === 0">You have not made any donations yet!</div>
         <div v-else>
       <div v-for="(donation, index) in donations" :key="index">
@@ -42,13 +63,52 @@
   </div>
   <br>
 
+      </div>
+      <div v-else>
+        <br>
+      <h1 class="header">Food Request History</h1>
+        <div v-if="requests.length === 0">You have not made any requests yet!</div>
+        <div v-else>
+      <div v-for="(request, index) in requests" :key="index">
+      <div v-for="(value, key) in request" :key="key">
+      <template v-if="key !== 'userEmail'">
+        <p>{{ key }}: {{ value }}</p>
+      </template>
+    </div>
+    <hr>
+    </div>
+  </div>
+  <br>
 
+        <!-- <h1 class="header">Food Request History</h1>
+        <div v-if="requests.length === 0">You have not made any requests yet!</div>
+        <table v-else>
+          <thead>
+            <tr>
+              <th>Bags of Rice</th>
+              <th>Canned Food</th>
+              <th>Packs of Instant Noodles</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(request, index) in requests" :key="index">
+              <td>{{ request.rice }}</td>
+              <td>{{ request.cannedFood }}</td>
+              <td>{{ request.instantNoodles }}</td>
+            </tr>
+          </tbody>
+        </table> -->
+        
+      </div>
+      <br>
       <button id="btn" @click="update()">Update Profile Details </button><br>
+      <!-- <button id="btn" @click="signOut()" v-if="user"> Logout </button>  -->
       <Logout/>
     </div>
-    <div v-else>
+  
+    <div v-else-if = "!exists">
+        <Details/>
     </div>
-
   </div>
 
   <div v-else class = "container2"> 
@@ -69,7 +129,7 @@ import { db } from '@/firebase'
 import { query, where, getDocs } from 'firebase/firestore';
 
 export default {
-  name: "BeneficiaryProfilePage", 
+  name: "DonorProfilePage", 
   components: {
     Logout, Details
   }, 
@@ -78,7 +138,10 @@ export default {
     return {
       user: false,
       exists: false,
-      donations: []
+      donations: [],
+      userData: false,
+      account: false,
+      requests: []
     }
   }, 
 
@@ -88,16 +151,18 @@ export default {
       if (user) {
         this.user = user;
         const docRef = doc(db, "User", this.user.displayName);
-        const docName = doc.name; 
         const docSnap = getDoc(docRef)
           .then((doc) => {
             if (doc.exists()) {
               console.log("Document data:", doc.data());
               this.exists = true;
-
+              this.userData = doc.data();
               // retrieve all instances from DonatedFood where userEmail matches current user's email
-              const donatededFoodRef = collection(db, "DonatedFood");
-              const q = query(donatededFoodRef, where("userEmail", "==", this.user.email));
+              
+              this.account = this.userData.type;
+              if (this.account=="Donor") {
+              const donatedFoodRef = collection(db, "DonatedFood");
+              const q = query(donatedFoodRef, where("userEmail", "==", this.user.email));
               const donatedFoodSnapshot = getDocs(q)
                 .then((querySnapshot) => {
                   const data = [];
@@ -106,13 +171,29 @@ export default {
                     data.push(doc.data());
                   });
                   this.donations = data;
+                })                
+                .catch((error) => {
+                  console.log("Error getting donated food documents: ", error);
+                });
+              } else {
+                const requestedFoodRef = collection(db, "RequestedFood");
+                const q = query(requestedFoodRef, where("userEmail", "==", this.user.email));
+                const requestedFoodSnapshot = getDocs(q)
+                .then((querySnapshot) => {
+                  const data = [];
+                  querySnapshot.forEach((doc) => {
+                    console.log(doc.id, " => ", doc.data());
+                    data.push(doc.data());
+                  });
+                  this.requests = data;
                 })
                 .catch((error) => {
                   console.log("Error getting donated food documents: ", error);
                 });
-
+              }
             } else {
               console.log("No such document!");
+
             }
           })
           .catch((error) => {
@@ -125,16 +206,15 @@ export default {
   methods: {
     update() {
       const auth = getAuth(); 
-      const user = auth.currentUser; 
-      router.push('/updatedonorprofile'); 
+      const user = auth.currentUser;
+      if (this.userData.type == "Donor") {
+        router.push('/updatedonorprofile')
+      } else {
+        router.push('/updatebeneficiaryprofile')
+      }
+      
     },
-    signOut() {
-      const auth = getAuth(); 
-      const user = auth.currentUser; 
-      signOut(auth, user);
-      router.push('/');
-      window.location.reload();
-    }
+
   }
 }
 </script>
@@ -143,7 +223,7 @@ export default {
 .container1 {
     background-color: aliceblue;
     width: 100vw;
-    height: 100vh;
+    height: 105vh;
     align-content: center;
     padding: 100px;
     font-family: Avenir, Arial, Helvetica, sans-serif;
