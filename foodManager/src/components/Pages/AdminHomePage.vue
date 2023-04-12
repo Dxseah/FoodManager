a95dff199dfd0800cad6fabb7e6e9b423937df01
Brian Liew <brianlw1872@gmail.com>
2023-04-13 02:56:25 +0800

    edited adminhomepage UI

--- foodManager/src/components/Pages/AdminHomePage.vue
+++ foodManager/src/components/Pages/AdminHomePage.vue
@@ -54,113 +54,78 @@ export default {
 }
 </script>
   
-  <style scoped>
-    div.background {
-      background: url(https://www.efsa.europa.eu/sites/default/files/styles/share_opengraph/public/news/food-donations.jpg?h=82f92a78&itok=3k3shqTm);
-      border: 2px solid black;
-      background-size: cover;
-      -webkit-background-size: cover;
-      -moz-background-size: cover;
-      -o-background-size: cover;
-      background-repeat: no-repeat;
-      height: 100vh;
-      width: 100vw;
-    }
-  
-    div.transbox {
-      margin: 30px;
-      background-color: #ffffff;
-      border: 1px solid black;
-      opacity: 0.8;
-    }
-  
-    div.content {
-    margin: 5%;
-    font-weight: bold;
-    color: #000000;
-  }
+<style scoped>
+div.background {
+  border: 2px solid black;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+  width: 100vw;
+}
 
-    .header {
-      font-family: Marker Felt, Avenir, Arial, Helvetica, sans-serif;
-      font-weight: bolder;
-      font-size: 3em;
-    }
+div.transbox {
+  margin: 30px;
+  background-color: #ffffff;
+  border: 1px solid black;
+  opacity: 0.8;
+}
 
-    .newHeader {
-      font-family: Marker Felt, Avenir, Arial, Helvetica, sans-serif;
-      font-weight: bolder;
-      font-size: 3em;
-    }
+div.content {
+margin: 5%;
+font-weight: bold;
+color: #000000;
+}
 
-    .food {
-        text-align: left;
-        margin-bottom: 8px;
-        font-weight: bolder;
-        font-size: 30px;
-    }
-
-    .percent {
-        text-align: right;
-        margin-top: 8px;
-        font-weight: bolder;
-        font-size: 25px;
-    }
-
-    .button-container {
-        display: flex;
-        justify-content: center;
-        margin-bottom: 16px; /* Add some margin to separate the button from the progress bar */
-    }
-
-#saveChangesBtn, .button {
-  display: inline-block;
-  padding: 12px 24px;
-  background-color: #00bfff;
-  color: #fff;
-  font-size: 24px; /* Increase the font size */
+.header {
+  font-family: Marker Felt, Avenir, Arial, Helvetica, sans-serif;
   font-weight: bold;
-  text-decoration: none;
-  border: none; /* Remove the border */
-  border-radius: 8px;
-  cursor: pointer;
-  transition: all 0.3s ease;
+  font-size: 3em;
+}
+.food {
+    text-align: left;
+    margin-bottom: 8px;
+    font-weight: bolder;
+    font-size: 30px;
+}
+
+.percent {
+    text-align: right;
+    margin-top: 8px;
+    font-weight: bolder;
+    font-size: 25px;
+}
+.button-container {
+display: flex;
+justify-content: center;
+margin-bottom: 16px; /* Add some margin to separate the button from the progress bar */
+}
+
+.button {
+display: inline-block;
+padding: 12px 24px;
+background-color: #00bfff;
+color: #fff;
+font-size: 24px; /* Increase the font size */
+font-weight: bold;
+text-decoration: none;
+border: none; /* Remove the border */
+border-radius: 8px;
+cursor: pointer;
+transition: all 0.3s ease;
 }
 
 /* Add hover styles */
-#saveChangesBtn:hover , .button:hover {
-  background-color: #0080ff;
-  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.3);
+.button:hover {
+background-color: #0080ff;
+box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.3);
 }
 
 /* Add active styles */
-#saveChangesBtn:active, .button:active{
-  transform: translateY(2px);
-  box-shadow: none;
+.button:active {
+transform: translateY(2px);
+box-shadow: none;
 }
-
-#deleteBtn {
-  display: inline-block;
-  padding: 12px 24px;
-  background-color: crimson;
-  color: #fff;
-  font-size: 24px; /* Increase the font size */
-  font-weight: bold;
-  text-decoration: none;
-  border: none; /* Remove the border */
-  border-radius: 8px;
-  cursor: pointer;
-  transition: all 0.3s ease;
-}
-
-/* Add hover styles */
-#deleteBtn:hover {
-  background-color: darkred;
-  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.3);
-}
-
-/* Add active styles */
-#deleteBtn:active {
-  transform: translateY(2px);
-  box-shadow: none;
-}
-  </style>
+</style>
