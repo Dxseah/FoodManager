fb633c3400087236e65bf1b537f6f97e8d81d41f
demitan02 <97874900+demitan02@users.noreply.github.com>
2023-04-16 01:16:54 +0800

    Update AdminShowDonationsPage.vue

--- foodManager/src/components/Pages/AdminShowDonationsPage.vue
+++ foodManager/src/components/Pages/AdminShowDonationsPage.vue
@@ -1,16 +1,16 @@
 <template>
-        <div class="background">
-      <div class="transbox">
+  <div class="background">
+    <div class="transbox">
         <div class="content">
           <h1 class="header">Check Donated items here</h1>
           <br>
           <div v-bind:key="item" v-for="item in this.donations">
             <img :src="item" alt="No Image" title="Check" />
           </div>
-          <router-link to="/adminhome">Back to Home Page</router-link>
+          <router-link to="/adminhome" class="routerbutton">Back to Home Page</router-link>
         </div>
-      </div>
     </div>
+  </div>
 </template>
 <script>
 import { getDoc, doc, updateDoc, setDoc, collection } from "firebase/firestore"
@@ -51,3 +51,31 @@ export default {
   }
 }
 </script>
+
+<style>
+.routerbutton {
+  background-color:#779ECB;
+  border-radius: 25px;
+  margin: 0px 0px 20px 0px;
+  display: inline-block;
+  padding: 12px 24px;
+  font-size: 18px; 
+  color: #fff;
+  font-weight: bold;
+  cursor: pointer;
+  border: none;
+  transition: all 0.3s ease;
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+}
+
+.routerbutton:hover {
+    background-color: #F6C7B3;
+    box-shadow: 3px 3px grey;
+    color: #2c3e50;
+}
+
+.routerbutton:active {
+  transform: translateY(2px);
+  box-shadow: none;
+}
+</style>
